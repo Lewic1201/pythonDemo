adbcb6407e6e173d4cf6effac004413906ab22d8
Lewic <lpf1201@qq.com>
2019-01-14 21:29:53 +0800

    add merge

--- source/leetcode/explore/lc_array.py
+++ source/leetcode/explore/lc_array.py
@@ -269,6 +269,38 @@ class Solution:
             res ^= i
         return res
 
+    @prints
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: void Do not return anything, modify nums1 in-place instead.
+
+        输入:
+        nums1 = [1,2,3,0,0,0], m = 3
+        nums2 = [2,5,6],       n = 3
+
+        输出: [1,2,2,3,5,6]
+        """
+        i = m + n - 1
+        while m > 0 and n > 0:
+            if nums1[m - 1] >= nums2[n - 1]:
+                nums1[i] = nums1[m - 1]
+                i -= 1
+                m -= 1
+            else:
+                nums1[i] = nums2[n - 1]
+                i -= 1
+                n -= 1
+        if n > 0:
+            while n > 0:
+                nums1[i] = nums2[n - 1]
+                i -= 1
+                n -= 1
+        return nums1
+
 
 if __name__ == '__main__':
     ss = Solution()
@@ -299,3 +331,6 @@ if __name__ == '__main__':
     #     [".", ".", ".", ".", "8", ".", ".", "7", "9"]
     # ]
     # ss.isValidSudoku(jgg)
+    ss.merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3)
+    ss.merge([1, 2, 3, 0, 0, 0], 3, [0, 5, 6], 3)
+    ss.merge([1, 2, 3, 0, 0, 0], 3, [1, 5, 6], 3)
