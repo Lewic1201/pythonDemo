9a6309968be7f4c0e8905c496ec80fc4a60d1930
Lewic <lpf1201@qq.com>
2019-01-27 01:08:45 +0800

    寻找重复数

---
+++ source/mediaTask/leetcode/explore/lc_arithmetic.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/1/27 0:19
+# @Author  : Administrator
+# @File    : lc_arithmetic.py
+# @Software: PyCharm
+# @context : 挑战算法
+
+
+from source.utils.decorators import print_cls
+
+
+class Solution:
+    @print_cls
+    def orderlyQueue(self, S, K):
+        """
+        :type S: str
+        :type K: int
+        :rtype: str
+        """
+
+
+
+if __name__ == '__main__':
+    ss = Solution()
+    ss.orderlyQueue()
