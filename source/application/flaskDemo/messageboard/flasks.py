eacf6037b044a46393510757b33036fe5264bd9e
lipanfeng <lpf1201@qq.com>
2019-01-28 17:03:06 +0800

    flask留言板

--- source/application/flaskDemo/messageboard/flasks.py
+++ source/application/flaskDemo/messageboard/flasks.py
@@ -5,14 +5,17 @@
 # @File    : hello.py
 # @Software: PyCharm
 
-from flask import Flask
+import os
+from flask import Flask, request, render_template
+from source.application.flaskDemo.messageboard.message import MessageManage
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'Index Page'
+    print(os.path.abspath('index.html'))
+    return render_template('index.html')
 
 
 @app.route('/hello')
@@ -20,35 +23,21 @@ def hello_world():
     return 'Hello World!'
 
 
-@app.route('/user/<username>')
-def show_user_profile(username):
-    # 显示用户的名称
-    return 'User %s' % username
+@app.route('/message', methods=['GET', 'POST'])
+def message():
+    # getData = request.args
+    # print('获取的get数据为:',getData)
+    post_data = request.form
+    print('获取的post数据为:', post_data)
+    username = request.form.get('username')
+    context = request.form.get('context')
+    mm = MessageManage()
+    mm.add_message(username, context)
+    mm.save_data()
 
-
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    # 显示提交整型的用户"id"的结果，注意"int"是将输入的字符串形式转换为整型数据
-    return 'Post %d' % post_id
-
-
-# @app.route('/login', methods=['GET', 'POST'])
-# def login():
-#     if request.method == 'POST':
-#         do_the_login()
-#     else:
-#         show_the_login_form()
-
-
-@app.route('/projects/')
-def projects():
-    return 'The project page'
-
-
-@app.route('/about')
-def about():
-    return 'The about page'
+    # send data
+    return render_template('board.html', datas=mm.datas[::-1])
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run("127.0.0.1", 5001)
--- source/application/flaskDemo/messageboard/message.py
+++ source/application/flaskDemo/messageboard/message.py
@@ -1,36 +1,70 @@
-from flask import Flask
-from flask import render_template
 import pickle
+import datetime
+import os
+import json
 
-app = Flask(__name__)
-
-
-@app.route('/')
-def index():
-    list1 = list(range(10))
-    my_list = [{"id": 1, "value": "我爱工作"},
-               {"id": 2, "value": "工作使人快乐"},
-               {"id": 3, "value": "沉迷于工作无法自拔"},
-               {"id": 4, "value": "日渐消瘦"},
-               {"id": 5, "value": "以梦为马，越骑越傻"}]
-    return render_template(
-        # 渲染模板语言
-        'index.html',
-        title='hello world',
-        list2=list1,
-        my_list=my_list
-    )
+ID_CREATER = ('{:0>5}'.format(i) for i in range(99999))
 
 
-# step1 定义过滤器
-def do_listreverse(li):
-    temp_li = list(li)
-    temp_li.reverse()
-    return temp_li
+class MessageManage:
+    def __init__(self, file='.\\datas'):
+        self.file = file
+        self.datas = self.get_data()
 
+    def get_data(self):
+        try:
+            if not os.path.exists(self.file):
+                return []
+            with open(self.file, 'rb') as files:
+                return pickle.load(files)
+        except:
+            return []
 
-# step2 添加自定义过滤器
-app.add_template_filter(do_listreverse, 'listreverse')
+    def add_message(self, username, context):
+        """
+        新增一条留言
+        :param username:
+        :param context:
+        :return:
+        """
+        # 生成一个id
+        ids = [int(data['mid']) for data in self.datas]
+        if not ids:
+            mid = '00000'
+        else:
+            mid = '{:0>5}'.format(max(ids) + 1)
+
+        if context is None or context.strip() is '':
+            return False, "留言不能为空!"
+        message = {
+            'mid': mid,
+            'now_time': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+            'username': username,
+            'context': context
+        }
+        self.datas.append(message)
+
+    def save_data(self):
+        with open(self.file, 'wb') as files:
+            pickle.dump(self.datas, files)
+
+    def get_console_data(self):
+        return json.dumps(self.datas)
+
+    def __str__(self):
+        ret = ''
+        for i in self.datas:
+            ret += '{} {} {:<10} {}\n'.format(i.get('mid'), i.get('now_time'), i.get('username'), i.get('context'))
+        return ret
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    mm = MessageManage()
+    # mm.add_message('zhang3', 'hello')
+    # mm.add_message('zhang34', 'hello')
+    # mm.add_message('zhang3', 'h2ello')
+    # mm.add_message('li4', 'h2el3lo')
+    # mm.add_message('liss4', 'h2el3lo')
+    # mm.save_data()
+    # print(mm)
+    print(mm.get_console_data())
