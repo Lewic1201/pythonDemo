929a6bbd921c2a8840d82b295778195b9f3ccd21
lipanfeng <lpf1201@qq.com>
2019-03-21 15:56:03 +0800

    msgboard-1.0.1.tar.gz 生成setup包

--- source/moudleDemo/thirdDemo/setupDemo/test2package/setup.py
+++ source/moudleDemo/thirdDemo/setupDemo/test2package/setup.py
@@ -1,12 +1,9 @@
 from setuptools import setup, find_packages
 
-# # 切到当前目录下,执行 python setup.py sdist
-# setup(name='test_myDemo')
-
 
 setup(
     name='msgboard',
-    version='1.0.0',
+    version='1.0.1',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
