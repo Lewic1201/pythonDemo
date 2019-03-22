f8b15e3d98ad1bbfe6a60786a895c6729d1b6c29
lipanfeng <lpf1201@qq.com>
2019-03-22 18:17:12 +0800

    english world

--- testFile/test.py
+++ testFile/test.py
@@ -32,3 +32,29 @@ print('\033[5;33;0m' + "[RESULT]:" + '\033[0m')
 #
 # a = os.path.join('a','b')
 # print(a)
+
+import re
+
+RE_HAS_CHINESE = [u".*[\u4e00-\u9fa5]+.*", '包含中文']
+
+pat1 = re.compile(u"([\u4e00-\u9fa5]+.*)")
+
+context = []
+with open('test.ini', 'rb') as ff:
+    try:
+        lines = ff.readlines()
+
+        for line in lines:
+            line = line.decode('utf-8')
+            zh = re.search(pat1, line)
+            if zh:
+                ss = zh.group()
+                line = line.replace(ss, '#$#' + ss)
+            else:
+                if line.strip() in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ':
+                    line = line[-3] * 19 + line
+            context.append(line)
+    except Exception:
+        raise
+with open('test', 'w') as ff:
+    ff.writelines(context)
