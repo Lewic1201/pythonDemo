57912c287b66499cf8c09c2946ea5878ca8f8467
li_panfeng <lewic@baba.com>
2019-06-18 08:49:58 +0800

    优化数据模板

--- source/moudleDemo/myTool/codeGenerate/ssm/template/ai-data-center/src/main/resources/start.sh
+++ source/moudleDemo/myTool/codeGenerate/ssm/template/ai-data-center/src/main/resources/start.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-nohup java -Xms128m -Xmx512m -XX:+HeapDumpOnOutOfMemoryError -jar ai-monitor-service-0.0.1-SNAPSHOT.jar &
+nohup java -Xms128m -Xmx512m -XX:+HeapDumpOnOutOfMemoryError -jar ai-data-center-0.0.1-SNAPSHOT.jar &
